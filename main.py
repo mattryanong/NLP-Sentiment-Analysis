7cfc656b59a17f8295b0128bc5249d4d4a55c490
Matthew Ong <e0309145@u.nus.edu>
2020-10-12 22:44:22 +0800

    edited mainpy for demo

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 import string
 from collections import Counter
-
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 
 # reading text file
+# encoding is to read stuff written in html
 text = open("read.txt", encoding="utf-8").read()
 
 # converting to lowercase
 lower_case = text.lower()
 
 # Removing punctuations
+# 1st param specifies list of chars that need to be replaced
+# 2nd param is the list of chars with which the chars need to be replaced
+# 3rd param specifies list of chars that need to be removed
 cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
 
 # splitting text into words
-tokenized_words = cleaned_text.split()
+# tokenized_words = cleaned_text.split()
+tokenized_words = word_tokenize(cleaned_text, "english")
 
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+# stop words don't add any meaning to the sentence (NLTK has a more exhaustive list)
+# stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+#               "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+#               "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+#               "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+#               "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+#               "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+#               "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+#               "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+#               "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+#               "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
 
 # Removing stop words from the tokenized words list
 final_words = []
 for word in tokenized_words:
-    if word not in stop_words:
+    if word not in stopwords.words('english'):
         final_words.append(word)
 
 # NLP Emotion Algorithm
@@ -42,9 +51,13 @@ for word in tokenized_words:
 # 3) Finally count each emotion in the emotion list
 
 emotion_list = []
+# 'r' is read only mode
 with open('emotions.txt', 'r') as file:
     for line in file:
+        # clean up all the unnecessary characters in each line
+        # strip is like .trim()
         clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        # splits line into whatever is before and after ":"
         word, emotion = clear_line.split(':')
 
         if word in final_words:
@@ -54,10 +67,27 @@ print(emotion_list)
 w = Counter(emotion_list)
 print(w)
 
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    print(score)
+    neg = score['neg']
+    pos = score['pos']
+    if neg > pos:
+        print("Negative Sentiment")
+    elif pos > neg:
+        print("Positive Sentiment")
+    else:
+        print("Neutral Sentiment")
+
+
+sentiment_analyse(cleaned_text)
+
 # Plotting the emotions on the graph
 
 fig, ax1 = plt.subplots()
 ax1.bar(w.keys(), w.values())
+# auto formats x and y axis so that all values are presented properly
 fig.autofmt_xdate()
 plt.savefig('graph.png')
 plt.show()
